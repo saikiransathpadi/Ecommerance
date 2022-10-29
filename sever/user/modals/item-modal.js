feb2b08aa091352354bacc013835e24fb07bb228
Sathpadi SaiKiran <sathpadi.saikiran@bijak.in>
2022-10-29 12:20:08 +0530

    custom

--- sever/user/modals/item-modal.js
+++ sever/user/modals/item-modal.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const itemSchema = mongoose.Schema({
-    item_name:String,
-    item_id: String,
-    item_catogery:String,
+    item_name: { type: String, required: true },
+    item_id: { type: String, unique: true },
+    item_category: { type: String, required: true },
     item_image: String,
-    actual_price: Number,
-    discounted_price: Number
-})
+    price: {type: Number, required: true},
+});
 
-
-const itemModal = mongoose.model("item",itemSchema);
+const itemModal = mongoose.model('item', itemSchema);
 module.exports = itemModal;
--- sever/user/routes/item.js
+++ sever/user/routes/item.js
@@ -1,20 +1,29 @@
-const express=require("express");
-const itemModal = require("../modals/item-modal");
+const express = require('express');
+const itemModal = require('../modals/item-modal');
+const uuid = require('uuid');
 
 const router = express.Router();
 
-
-router.get("/",(req,res)=>{
-    itemModal.find().then((itemData)=>{
-        res.status(200).send({item : itemData})
-    })
-});
-
-router.post("/add", (req, res)=> {
-    itemModal.insertMany(req.body.items).then((itemData)=> {
-        res.status(200).send("Data Added Successfully");
+router.get('/', (req, res) => {
+    itemModal.find().then((itemData) => {
+        res.status(200).send({ item: itemData });
     });
 });
 
+router.post('/add', (req, res) => {
+    itemModal
+        .insertMany([
+            ...req.body.items.map((item) => {
+                return {
+                    ...item,
+                    item_id: uuid.v4(),
+                };
+            }),
+        ])
+        .then((itemData) => {
+            res.status(200).send('Data Added Successfully');
+        })
+        .catch((e) => res.status(400).send(e.message));
+});
 
 module.exports = router;
